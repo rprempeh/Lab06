af1ef66f11d3756cd8ba6c29ef9b06934ed97830
tpope98 <travispope98@outlook.com>
2023-07-19 23:34:30 -0400

    Edits by David

--- lab06.py
+++ lab06.py
@@ -5,7 +5,7 @@ def encode(password):
         if len(encoded_num) > 1:
             encoded_num = encoded_num[-1]
         encoded += encoded_num
-    return encodeddef encode(password):
+    return encoded
     for num in password:
         encoded = ""
         encoded_num = int(num) + 3
@@ -13,6 +13,17 @@ def encode(password):
             encoded_num = encoded_num[-1]
         encoded += encoded_num
     print("Your password has been encoded and stored.")
+
+
+def decode(key_encoded):  # This is David. Hopefully it doesn't say travis this time when I push.
+    decoded = ''
+    for num in encoded:
+        decoded_num = int(num) - 3
+        if decoded_num < 0:
+            decoded_num += 10
+        decoded += str(decoded_num)
+    return decoded
+
 
 def main():
     proceed = True
@@ -26,12 +37,11 @@ def main():
             key_encoded = encode(passkey)
             print("Your password has been encoded and stored.")
             print()
-            if menu option == 2:
-                decoded_key = decode(key_encoded)
-                print(f"The encoded password is {key_encoded}, and the original password is {decoded_key}.")
-            if menu_option == 3:
-                proceed = False
-
+        if menu_option == 2:
+            decoded_key = decode(key_encoded)
+            print(f"The encoded password is {key_encoded}, and the original password is {decoded_key}.\n")
+        if menu_option == 3:
+            proceed = False
 
 
 if __name__ == '__main__':
